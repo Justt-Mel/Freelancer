8ab620ca7df731e877c738eb4a68382d53e03eb2
Justt-Mel <justinmelendezj@gmail.com>
2025-03-26 11:41:48 -0400

    Created 2 arrays with names and occupations, created a random price generator, and then created a function that creates a random freelancer.

--- freeLance.js
+++ freeLance.js
@@ -1,3 +1,18 @@
-const freeLancers = {
+const jobs = ["Programmer","Tutor","Artist","Lawyer","Accountant","Photographer","Driver","Gardener"];
 
+const person = ["James","Amy","Sam","Justin","Ethan","Jeff","Frank","Liz","Sophia","Claire"];
+
+const price = Math.floor(Math.random()*(100-30+1))+30;
+
+function randFree(){
+    const freeJob = Math.floor(Math.random()* jobs.length);
+    const randPer = Math.floor(Math.random()* person.length);
+    const freeLancers = {
+        per: person[randPer],
+        career: jobs[freeJob],
+        wage: price
+    }
+    console.log(freeLancers)
 }
+randFree()
+
